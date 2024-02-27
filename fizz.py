53645f772a99722f154b478c90d02110a982a1b1
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 16:23:11 -0300

    Create fizz.py

---
+++ fizz.py
@@ -0,0 +1,8 @@
+# Recebendo o número inteiro na entrada
+numero = int(input("Digite um número inteiro: "))
+
+# Verificando se o número é divisível por 3 e imprimindo "Fizz" ou o número
+if numero % 3 == 0:
+    print("Fizz")
+else:
+    print(numero)
