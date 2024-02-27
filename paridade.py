1e5941c0be897fa55655910522ff449a593c4b78
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 16:22:07 -0300

    Create paridade.py

---
+++ paridade.py
@@ -0,0 +1,8 @@
+# Recebendo o número inteiro na entrada
+numero = int(input("Digite um número inteiro: "))
+
+# Verificando se o número é par ou ímpar e imprimindo a mensagem correspondente
+if numero % 2 == 0:
+    print("par")
+else:
+    print("ímpar")
