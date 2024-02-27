11fb08c46879e370d3c7ee7488144c81560f71f7
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 16:24:09 -0300

    Create buzz.py

---
+++ buzz.py
@@ -0,0 +1,8 @@
+# Recebendo o número inteiro na entrada
+numero = int(input("Digite um número inteiro: "))
+
+# Verificando se o número é divisível por 5 e imprimindo "Buzz" ou o número
+if numero % 5 == 0:
+    print("Buzz")
+else:
+    print(numero)
