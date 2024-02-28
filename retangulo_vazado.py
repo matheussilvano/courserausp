38a9079fef43b1cca552818c16d84db85ac93d90
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 22:43:05 -0300

    Create retangulo_vazado.py

---
+++ retangulo_vazado.py
@@ -0,0 +1,11 @@
+# Exercício 2
+largura = int(input("Digite a largura: "))
+altura = int(input("Digite a altura: "))
+
+for i in range(altura):
+    for j in range(largura):
+        if i == 0 or i == altura - 1 or j == 0 or j == largura - 1:
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print()  # Quebra de linha após cada linha do retângulo
