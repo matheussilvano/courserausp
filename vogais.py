63bd786c5714321009ff28fe3e0fcbb66fc5ce0b
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 22:32:04 -0300

    Create vogais.py

---
+++ vogais.py
@@ -0,0 +1,7 @@
+def vogal(caractere):
+    return caractere.lower() in 'aeiou'
+
+# Exemplos de execução
+print(vogal("a"))  # Saída: True
+print(vogal("b"))  # Saída: False
+print(vogal("E"))  # Saída: True
