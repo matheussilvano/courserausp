d64ff97c8b9cd48289225769458c2fb1aed83e24
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 22:13:33 -0300

    Update n_impares.py

--- n_impares.py
+++ n_impares.py
@@ -3,8 +3,6 @@ n = int(input("Digite o valor de n: "))
 contador = 0
 numero = 1
 
-print("Os", n, "primeiros números ímpares naturais são:")
-
 while contador < n:
     print(numero)
     numero += 2
