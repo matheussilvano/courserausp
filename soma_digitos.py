1d1ca886a3ce43f8e8e641321e7f8c0dc965dea8
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2024-02-27 22:19:17 -0300

    Update soma_digitos.py

--- soma_digitos.py
+++ soma_digitos.py
@@ -1,10 +1,7 @@
 numero = input("Digite um número inteiro: ")
-
 soma_digitos = 0
-
 # Itera sobre cada dígito do número
 for digito in numero:
     # Converte o dígito de string para inteiro e adiciona à soma
     soma_digitos += int(digito)
-
 print(soma_digitos)
